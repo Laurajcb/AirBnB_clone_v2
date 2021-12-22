557008dd4bab97010657829207b6eb49d332076f
anversa-pro <angelavergaras@gmail.com>
2021-12-22 15:27:33 -0500

    [MODIFY] add tests & fix typing err

--- models/__init__.py
+++ models/__init__.py
@@ -4,7 +4,7 @@
 from os import getenv
 
 
-if getenv('HBNB_TYPE_STORAGES') == 'db':
+if getenv('HBNB_TYPE_STORAGE') == 'db':
     from models.engine.db_storage import DBStorage
     storage = DBStorage()
 else:
