87cec79498ed19f64afdaec1924910674ce00300
Laurajcb <3398@holbertonschool.com>
2021-12-22 15:58:30 -0500

    remove line on dict

--- models/base_model.py
+++ models/base_model.py
@@ -58,7 +58,6 @@ class BaseModel():
         """Convert instance into dict format"""
         dictionary = {}
         dictionary.update(self.__dict__)
-        print('update:{}'.format(self.__dict__))
         dictionary.update({'__class__':
                           (str(type(self)).split('.')[-1]).split('\'')[0]})
         dictionary['created_at'] = self.created_at.isoformat()
