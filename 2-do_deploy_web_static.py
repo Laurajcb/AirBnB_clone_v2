6b8e8a20a589a16d07688f60a175f668feb98451
laurajcb <callejaslaura96@gmail.com>
2022-01-13 20:51:47 +0000

    trying fix

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -23,27 +23,21 @@ def do_pack():
 
 
 def do_deploy(archive_path):
-    """ Distributes an archive to your web servers. """
-
-    if not os.path.exists(archive_path):
-        return False
-
-    path = "/data/web_static/releases/"
-    name = archive_path.split('.')[0].split('/')[1]
-    dest = path + name
-
+    """Deploy the boxing package tgz file
+    """
     try:
+        archive = archive_path.split('/')[-1]
+        path = '/data/web_static/releases/' + archive.strip('.tgz')
+        current = '/data/web_static/current'
         put(archive_path, '/tmp')
-        run('mkdir -p {}'.format(dest))
-
-        run('tar -xzf /tmp/{}.tgz -C {}'.format(name, dest))
-        run('rm -f /tmp/{}.tgz'.format(name))
-        run('mv {}/web_static/* {}/'.format(dest, dest))
-        run('rm -rf {}/web_static'.format(dest))
-        run('rm -rf /data/web_static/current')
-        run('ln -s {} /data/web_static/current'.format(dest))
-
+        run('mkdir -p {}/'.format(path))
+        run('tar -xzf /tmp/{} -C {}'.format(archive, path))
+        run('rm /tmp/{}'.format(archive))
+        run('mv {}/web_static/* {}'.format(path, path))
+        run('rm -rf {}/web_static'.format(path))
+        run('rm -rf {}'.format(current))
+        run('ln -s {} {}'.format(path, current))
+        print('New version deployed!')
         return True
-
     except:
         return False
