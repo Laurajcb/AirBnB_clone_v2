73de8cdcae7912184b601da0248f89d2b3071911
laurajcb <callejaslaura96@gmail.com>
2022-01-13 19:48:48 +0000

    added new lines

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -8,6 +8,18 @@ import os
 env.hosts = ["34.75.150.142", "	54.205.3.49"]
 
 
+def do_pack():
+    """ Function to generate a tgz from web_static"""
+    try:
+        local("mkdir -p versions")
+        date = datetime.now().strftime('%Y%m%d%H%M%S')
+        tgz_file = "versions/web_static_{}.tgz".format(date)
+        local("tar -czvf {} web_static".format(tgz_file))
+        return tgz_file
+    except:
+        return None
+
+
 def do_deploy(archive_path):
     """ Distributes an archive to your web servers. """
 
@@ -21,13 +33,15 @@ def do_deploy(archive_path):
     try:
         put(archive_path, '/tmp')
         run('mkdir -p {}'.format(dest))
+
         run('tar -xzf /tmp/{}.tgz -C {}'.format(name, dest))
         run('rm -f /tmp/{}.tgz'.format(name))
         run('mv {}/web_static/* {}/'.format(dest, dest))
         run('rm -rf {}/web_static'.format(dest))
         run('rm -rf /data/web_static/current')
         run('ln -s {} /data/web_static/current'.format(dest))
+
         return True
-    
+
     except:
         return False
