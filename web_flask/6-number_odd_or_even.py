29db7fae090e436ba6b77bcc3dbfece774e730c4
laurajcb <callejaslaura96@gmail.com>
2022-01-29 18:59:09 +0000

    delete line

--- web_flask/6-number_odd_or_even.py
+++ web_flask/6-number_odd_or_even.py
@@ -31,8 +31,7 @@ def p_messages(text):
 @app.route('/number/<int:n>', strict_slashes=False)
 def n_messages(n):
     """ Function that displays is a nunber with a giving number """
-    if n.isnumeric():
-        return '{} is a nuber'.format(n)
+    return '{} is a number'.format(n)
 
 @app.route('/number_template/<int:n>', strict_slashes=False)
 def template_messages(n):
