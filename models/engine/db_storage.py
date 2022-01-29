b9af653147a7d75247f8bff6d64f369aaad2c616
laurajcb <callejaslaura96@gmail.com>
2022-01-29 19:06:11 +0000

    change some lines

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -65,21 +65,16 @@ class DBStorage():
             self.__session.delete(obj)
         self.save()
 
-    def reload(self):
-        """reload the session"""
-        from models.base_model import BaseModel, Base
-        from models.amenity import Amenity
-        from models.city import City
-        from models.place import Place
-        from models.review import Review
-        from models.state import State
-        from models.user import User
+    ef reload(self):
+        """ Create all tables in the database """
         Base.metadata.create_all(self.__engine)
+
         session_factory = sessionmaker(
-            bind=self.__engine, expire_on_commit=False)
-        self.__session = scoped_session(session_factory)
+                bind=self.__engine, expire_on_commit=False)
+        Session = scoped_session(session_factory)
 
+        self.__session = Session()
 
-  def close(self):
-        """Function to close the active session """
+    def close(self):
+        """ Close the session """
         self.__session.close()
--- models/state.py
+++ models/state.py
@@ -1,25 +1,31 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
+
+from models.city import City
 from models.base_model import BaseModel, Base
-from sqlalchemy import Column, String, ForeignKey
 from sqlalchemy.orm import relationship
+from sqlalchemy import Column, String
+from os import getenv
 
 
 class State(BaseModel, Base):
     """ State class """
-    __tablename__ = "states"
-    name = Column(String(128), nullable=False)
-    cities = relationship('City', backref='states')
 
-    @property
-    def cities(self):
-        """ getter attribute cities that returns the list of City
-        instances with state_id equals to the current State.id"""
-        from models import storage
-        from models.city import City
-        list = []
-        cities = storage.all(City)
-        for value in cities.values():
-            if value.state_id == self.id:
-                list.append(value)
-        return list
+    __tablename__ = 'states'
+    name = Column(String(128), nullable=False)
+
+    if getenv("HBNB_TYPE_STORAGE") == "db":
+        cities = relationship("City", backref="state", cascade="all")
+
+    else:
+        def cities(self):
+            """ Return the list of City instances with state_id """
+            from models import storage
+            city_list = []
+
+            for ct in storage.all(City).values():
+                if ct.state_id == self.id:
+                    city_list.append(ct)
+
+            return city_list
+            
