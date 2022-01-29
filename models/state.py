4edde5bcda9fc92850cc583d1806a9aac93e260e
laurajcb <callejaslaura96@gmail.com>
2022-01-29 19:14:41 +0000

    pep8

--- models/state.py
+++ models/state.py
@@ -1,31 +1,26 @@
 #!/usr/bin/python3
-""" State Module for HBNB project """
+""" State Module for HBNB project for AirBNB_clone_v2"""
 
-from models.city import City
+
 from models.base_model import BaseModel, Base
-from sqlalchemy.orm import relationship
 from sqlalchemy import Column, String
-from os import getenv
+from sqlalchemy.orm import relationship
+from models.engine.file_storage import FileStorage
 
 
 class State(BaseModel, Base):
-    """ State class """
-
+    """ State class definition in the next line"""
     __tablename__ = 'states'
     name = Column(String(128), nullable=False)
+    cities = relationship('City', backref='state')
 
-    if getenv("HBNB_TYPE_STORAGE") == "db":
-        cities = relationship("City", backref="state", cascade="all")
-
-    else:
-        def cities(self):
-            """ Return the list of City instances with state_id """
-            from models import storage
-            city_list = []
-
-            for ct in storage.all(City).values():
-                if ct.state_id == self.id:
-                    city_list.append(ct)
-
-            return city_list
-            
+    @property
+    def cities(self):
+        """Getter attribute cities that returns the list of City instances"""
+        from models import storage
+        from models.city import City
+        new_list = []
+        for key, obj_city in storage.all(City).items():
+            if obj_city.state_id == self.id:
+                new_list.append(obj_city)
+        return new_list
