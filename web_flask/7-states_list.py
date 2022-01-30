9f38c13087d852ee9d7760619b775d9747ed4303
laurajcb <callejaslaura96@gmail.com>
2022-01-30 22:46:47 +0000

    added point 8 & 9

---
+++ web_flask/7-states_list.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+""" Star a Flask web application for Airbnb_clone,
+    and connecting to the database """
+
+
+from flask import Flask, render_template
+from models import storage
+from models.state import State
+
+app = Flask(__name__)
+
+@app.route('/states_list', strict_slashes=False)
+def states_template():
+    """Function to return display a template of states from db"""
+    states = storage.all(State)
+    return render_template('7-states_list.html', States=states.values())
+
+
+@app.teardown_appcontext
+def teardown(exception):
+    """ Remove the current SQLAlchemy Session"""
+    storage.close()
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
